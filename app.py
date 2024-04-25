ec0946bd867dc724c5f16d662400e9d2cc0509c8
silviobis <primoboby@hotmail.com>
2024-04-25 05:47:03 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,36 @@
+import openpyxl
+from urllib.parse import quote
+import webbrowser
+from time import sleep
+import pyautogui
+
+webbrowser.open('https://web.whatsapp.com/')
+sleep(30)
+
+#Ler Planilha e guardar informações sobre nome, telefone e data
+workbook = openpyxl.load_workbook('contatos_chat_bot.xlsx')
+pagina_clientes = workbook['Planilha1']
+
+for linha in pagina_clientes.iter_rows(min_row=2):
+    #variaveis nome,telefone, data
+    nome = linha[0].value
+    telefone = linha[1].value
+    data = linha[2].value
+
+    mensagem = f'Olá {nome} Sou um chatbot desenvolvido em python(linguagem de programação) Voce tem até o dia {data.strftime("%d/%m/%Y")}. Para me responder se recebeu a mensagem e se gostou e deixar o like na minha publicação que vou deixar o link https://link_do_post_.com Obs. é apenas um teste'
+    
+    #Criar links personalizados do whatsapp e enviar mensagens para cada cliente com base nos dados da planilha
+    try:
+        link_mensagem_whatsapp = f'https://web.whatsapp.com/send?phone={telefone}&text={quote(mensagem)}'
+        webbrowser.open(link_mensagem_whatsapp)
+        sleep(10)
+        seta = pyautogui.locateCenterOnScreen('seta.png')
+        sleep(5)
+        pyautogui.click(seta[0],seta[1])
+        sleep(5)
+        pyautogui.hotkey('ctrl','w')
+        sleep(5)
+    except:
+        print(f'Não foi possivel enviar mensagem para {nome}')
+        with open('erros.csv','a',newline='',encoding='utf-8') as arquivo:
+            arquivo.write(f'{nome},{telefone}')   
---
+++ readme.md
@@ -0,0 +1,24 @@
+"""DESCREVER OS PASSOS MANUAIS E TRANSFORMAR ISSO EM CÓDIGO"""
+
+#Como automatizar esse processo?
+-Onde está feito?(Versão Web)
+-Quais tec. preciso pra resolver essa demanda?
+	-Teclado (Pyautogui)
+	-Acesso ao site(webbrowser)
+	-Automatizar digitação(link whatsapp)
+	-Automatizar Leitura de dados de uma planilha(openpyxl)
+
+
+
+#Ler planilha e guardar informações sobre Nome,Telefone e Mensagem
+#Criar links Personalizados do whatsapp e enviar mensagem para cada Contato
+#Com base nos dados da Planilha
+
+1-Instalar Openpyxl
+    Windows
+        pip install openpyxl
+    Linux ou mac
+        pip3 install openpyxl
+
+
+
